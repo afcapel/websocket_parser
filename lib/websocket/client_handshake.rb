07b80f7534ee4ac62e52948aeb4042b531123af6
Alberto Fernandez-Capel <afcapel@gmail.com>
2012-08-08 18:18:59 +0200

    Make accept token generation a class method.
    So if you already have a mechanism to handle http requests you
    can fecth the token without creating a handshake object.

--- lib/websocket/client_handshake.rb
+++ lib/websocket/client_handshake.rb
@@ -3,6 +3,10 @@ require 'digest/sha1'
 module WebSocket
   class ClientHandshake < Http::Request
 
+    def self.accept_token_for(websocket_key)
+      Digest::SHA1.base64digest(websocket_key.strip + GUID)
+    end
+
     def initialize(method, uri, headers = {}, proxy = {}, body = nil, version = "1.1")
       @method = method.to_s.downcase.to_sym
       @uri = uri.is_a?(URI) ? uri : URI(uri.to_s)
@@ -21,16 +25,10 @@ module WebSocket
       response_headers = {
         'Upgrade'    => 'websocket',
         'Connection' => 'Upgrade',
-        'Sec-WebSocket-Accept' => accept_token
+        'Sec-WebSocket-Accept' => ClientHandshake.accept_token_for(headers['Sec-WebSocket-Key'])
       }
 
       ServerHandshake.new(101, '1.1', response_headers)
-    end
-
-    private
-
-    def accept_token
-      Digest::SHA1.base64digest(headers['Sec-WebSocket-Key'].strip + GUID)
     end
   end
 end
