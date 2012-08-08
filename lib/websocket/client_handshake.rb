5060104bf89921b404f331ac849c062893c373be
Alberto Fernandez-Capel <afcapel@gmail.com>
2012-08-09 00:34:02 +0200

    Add validation errors to client handshake

--- lib/websocket/client_handshake.rb
+++ lib/websocket/client_handshake.rb
@@ -7,25 +7,37 @@ module WebSocket
       Digest::SHA1.base64digest(websocket_key.strip + GUID)
     end
 
-    def initialize(method, uri, headers = {}, proxy = {}, body = nil, version = "1.1")
-      @method = method.to_s.downcase.to_sym
-      @uri = uri.is_a?(URI) ? uri : URI(uri.to_s)
-
-      @headers = headers
-      @proxy, @body, @version = proxy, body, version
+    def errors
+      @errors ||= []
     end
 
     def valid?
-      headers['Connection'] == 'Upgrade'   &&
-      headers['Upgrade']    == 'websocket' &&
-      headers['Sec-WebSocket-Version'].to_i == PROTOCOL_VERSION
+      if headers['Connection'].downcase != 'upgrade'
+        errors << 'Not connection upgrade'
+        return false
+      end
+
+      if headers['Upgrade'].downcase != 'websocket'
+        errors << 'Connection upgrade is not for websocket'
+        return false
+      end
+
+      # Careful: Http gem changes header capitalization,
+      # so Sec-WebSocket-Version becomes Sec-Websocket-Version
+      if headers['Sec-Websocket-Version'].to_i != PROTOCOL_VERSION
+        errors << "Protocol version not supported '#{headers['Sec-Websocket-Version']}'"
+        return false
+      end
+
+      return true
     end
 
     def accept_response
+      websocket_key = headers['Sec-Websocket-Key']
       response_headers = {
         'Upgrade'    => 'websocket',
         'Connection' => 'Upgrade',
-        'Sec-WebSocket-Accept' => ClientHandshake.accept_token_for(headers['Sec-WebSocket-Key'])
+        'Sec-WebSocket-Accept' => ClientHandshake.accept_token_for(websocket_key)
       }
 
       ServerHandshake.new(101, '1.1', response_headers)
