438ae780319cb7c3897aaa13c7d130b0823829bb
Alberto Fernandez-Capel <afcapel@gmail.com>
2012-08-08 14:45:56 +0200

    Add method to render handshake response

--- lib/websocket/server_handshake.rb
+++ lib/websocket/server_handshake.rb
@@ -1,9 +1,23 @@
 module WebSocket
   class ServerHandshake < Http::Response
 
-    def initialize(status = nil, version = "1.1", headers = {}, body = nil, &body_proc)
+    def initialize(status = 101, version = "1.1", headers = {}, body = nil, &body_proc)
       @status, @version, @body, @body_proc = status, version, body, body_proc
       @headers = headers
+    end
+
+    def render(out)
+      response_header = "#{@version} #{@status} #{@reason}#{CRLF}"
+
+      unless @headers.empty?
+        response_header << @headers.map do |header, value|
+          "#{header}: #{value}"
+        end.join(CRLF) << CRLF
+      end
+
+      response_header << CRLF
+
+      out << response_header
     end
   end
 end
